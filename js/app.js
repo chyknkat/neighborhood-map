8490230949db5800e802d2f1c48fd63286a48e8f
Katrina <katrina@Katrinas-MacBook-Air.local>
2016-01-14 17:53:04 -0500

    refactor: Try to add markers to map

--- js/app.js
+++ js/app.js
@@ -1,51 +1,89 @@
-var Location = function(data){
-	this.locationName = ko.observable(data.locationName);
+//Initialize Google Map 
+var map;
+var marker
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 26.138761, lng: -80.159683}
+  });
 }
 
-var initialLocations = [
+var initialLocations = ko.observableArray([
 	{
-		locationName: "Funky Buddha"
+		locationName: "Funky Buddha",
+		lat: 26.1746495,
+		lng: -80.1308203
 	},
 	{
-		locationName: "Museum of Discovery and Science"
+		locationName: "Museum of Discovery and Science",
+		lat: 26.1208918,
+		lng: -80.1479404
 	},
 	{
-		locationName: "The Grilled Cheese"
+		locationName: "New York Grilled Cheese",
+		lat: 26.1564468,
+		lng: -80.1385884
 	},
 	{
-		locationName: "Broward Center for Performing Arts"
+		locationName: "Broward Center for Performing Arts",
+		lat: 26.1195189,
+		lng: -80.1490456
 	},
 	{
-		locationName: "Las Olas"
+		locationName: "Historic Stranahan House Museum",
+		lat: 26.1185494,
+		lng: -80.1373629
 	}
-]
+]);
+
+for(var i = 0; i < initialLocations().length; i++){
+	    var location = initialLocations()[i];
+	    var latLng = new google.maps.LatLng(location.lat, location.lng);
+	  	marker = new google.maps.Marker({
+			map: map,
+			animation: google.maps.Animation.DROP,
+			position: latLng
+	    });
+	    marker.addListener('click', toggleBounce);
+	};
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+var Location = function(data){
+	this.locationName = ko.observable(data.locationName);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+	
+}
+
+
 var ViewModel = function (){
 	var self = this;
 
 	this.locationList = ko.observableArray([]);
 	
-	initialLocations.forEach(function(locationItem){
+	initialLocations().forEach(function(locationItem){
 		self.locationList.push (new Location(locationItem));
 	});
+	
 
 	this.currentLocation = ko.observable(this.locationList()[0]);
 
-	this.setLocation = function (){
-		console.log("hi");
+	this.setLocation = function (clickedLocation){
+		self.currentLocation(clickedLocation);
 	};
 }
 
 ko.applyBindings(new ViewModel());
 
-//Initialize Google Map 
 
-	var map;
-	function initMap() {
-	  map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: 26.138761, lng: -80.159683},
-	    zoom: 13
-	  });
-	}
+
 
 // Map markers
 // Search bar
