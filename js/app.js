f26005b09a7e791fcdbb4f2f11c52df714a7a5e1
Katrina <katrina@Katrinas-MacBook-Air.local>
2016-01-29 15:31:30 -0500

    feat: Add info windows when list items are clicked, add error handlers, and start ajax request to Wikipedia

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 //Model array with initial locations
 var initialLocations = [
 	{
-		title: "Funky Buddha",
-		lat: 26.1746495,
-		lng: -80.1308203,
-		address: "1201 NE 38th St, Oakland Park, FL 33334"
+		title: "Fort Lauderdale Swap Shop",
+		lat: 26.137943, 
+		lng: -80.189965,
+		address: "3291 W Sunrise Blvd, Fort Lauderdale, FL 33311"
 
 	},
 	{
@@ -16,21 +16,21 @@ var initialLocations = [
 		
 	},
 	{
-		title: "New York Grilled Cheese",
-		lat: 26.1564468,
-		lng: -80.1385884,
-		address: "2207 Wilton Dr, Wilton Manors, FL 33305"
+		title: "Stonewall National Museum and Archives",
+		lat: 26.136952,
+		lng: -80.130058,
+		address: "1300 E Sunrise Blvd, Fort Lauderdale, FL 33304"
 		
 	},
 	{
-		title: "Broward Center for Performing Arts",
+		title: "Broward Center for the Performing Arts",
 		lat: 26.1195189,
 		lng: -80.1490456,
 		address: "201 SW 5th Ave, Fort Lauderdale, FL 33312"
 		
 	},
 	{
-		title: "Historic Stranahan House Museum",
+		title: "Stranahan House",
 		lat: 26.1185494,
 		lng: -80.1373629,
 		address: "335 SE 6th Ave, Fort Lauderdale, FL 33301"
@@ -38,59 +38,113 @@ var initialLocations = [
 	}
 ];
 
-
+//Globals
 var map;
 var marker;
 var markers = [];
 var latLng;
 var infowindow;
 var infowindows = [];
+var InfoWindowHTML;
+
 
 //Initialize Google Map 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: {lat: 26.138761, lng: -80.159683}
-  });
+	try {
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 13,
+	    center: {lat: 26.121745, lng: -80.156641}
+	  });
 
-  // Create markers for the initial locations
-  for(var i = 0; i < initialLocations.length; i++){
-  	var loc = initialLocations[i];
-  	latLng = new google.maps.LatLng(loc.lat, loc.lng);
-  	marker = new google.maps.Marker({
-  		animation: google.maps.Animation.DROP,
-  		position: latLng
-  	});
+	  // Create markers for the initial locations
+	  for(var i = 0; i < initialLocations.length; i++){
+	  	var loc = initialLocations[i];
+	  	latLng = new google.maps.LatLng(loc.lat, loc.lng);
+	  	marker = new google.maps.Marker({
+	  		animation: google.maps.Animation.DROP,
+	  		position: latLng
+	  	});
 
-  	infowindow = new google.maps.InfoWindow({
-	    content: "Hi"
-	});
+	  	//Link markers to map
+	  	marker.setMap(map);
 
-  	//Link markers to map
-  	marker.setMap(map);
+	  	//link markers to locations 
+		loc.marker = marker;
 
-  	//link markers to locations 
-	loc.marker = marker;
+		//push markers into array for later access
+		markers.push(marker);
 
-	//push markers into array for later access
-	markers.push(marker);
+	  	//Listener so that when markers are clicked, they bounce
+		marker.addListener('click', toggleBounce); 
 
-  	//Listener so that when markers are clicked, they bounce
-	marker.addListener('click', toggleBounce); 
+		//create info windows
+		infowindow = new google.maps.InfoWindow();
 
-	loc.infowindow = infowindow;
+		//link info windows to locations
+		loc.infowindow = infowindow;
 
-	infowindows.push(infowindow);
+		//push info windows into an array for later access
+		infowindows.push(infowindow);
 
-	marker.addListener('click', function() {
-	    infowindow.open(map, this);
-	});
-  }
+		//listener so when markers are clicked, Wikipedia loads
+		marker.addListener('click', function() {
+			loadWikipedia(this);
+		    
+		});
+
+  	  }
+  	//error handler for map
+	} catch (error) {
+		alert("An error occured loading the map.");
+	};
   //Activate Knockout and link data bindings to the ViewModel
   ko.applyBindings(new ViewModel());
 }
 
-//Bounce funtion for markers with 3 second timeout
+//loads Wikipedia when a marker is clicked
+function loadWikipedia(clickedMarker) {
+	//gets lat and long of the clicked marker
+	latLng = clickedMarker.getPosition();
+	var lat = latLng.lat().toFixed(7);
+	var lng = latLng.lng().toFixed(7); 
+
+	//loop to compare lat and long of clicked marker to their locations
+	for (var i = 0; i < initialLocations.length; i++) {
+		if (initialLocations[i].lat == lat && initialLocations[i].lng == lng) {
+			//sends request to Wikipedia
+			var url = "https://en.wikipedia.org/w/api.php?action=query&titles=" + initialLocations[i].title + "&prop=revisions&rvprop=content&format=json"
+			$.ajax({
+				url: url,
+				type: "POST",
+				dataType: "jsonp", 
+				headers: { 'Api-User-Agent': 'Katrina/1 (chykn75@yahoo.com)' },
+				//displays Wikipedia info in infowindow
+				success: function(result){
+					if (result!== null) {
+						console.log(result);
+					};
+			        //var object = JSON.parse(result);
+
+			        InfoWindowHTML = "WooHoo! Wikipedia";
+			        createInfoWindow(clickedMarker);
+			    },
+			    //error handler for Wikipedia
+			    error: function(error) {
+			    	console.log("You suck" + error);
+			    	InfoWindowHTML = "Unable to connect to Wikipedia";
+			    	createInfoWindow(clickedMarker);
+			    }
+			});
+		}
+	}
+}
+
+//opens info window after Wikipedia loads
+function createInfoWindow(clickedMarker){
+	infowindow.setContent(InfoWindowHTML);
+	infowindow.open(map, clickedMarker);
+}
+//Bounce funtion for markers with 1 second timeout
 function toggleBounce() {
 	var self = this;
 	if (this.getAnimation() !== null) {
@@ -99,7 +153,7 @@ function toggleBounce() {
 	  	this.setAnimation(google.maps.Animation.BOUNCE);
 	  	setTimeout(function() {
 	  		self.setAnimation(null)
-	  	}, 2000);
+	  	}, 1000);
 	}
 };
 
@@ -129,12 +183,10 @@ var ViewModel = function (){
 	//sets the current location to the first location in the locationList array
 	this.currentLocation = ko.observable(this.locationList()[0]);
 
-	//Got help from Udacity Coach Heidi for binding toggleBounce
-	//sets the current location to the location clicked and binds marker bounce to location click
+	//sets the current location to the location clicked and binds markers to location click
 	this.setLocation = function (clickedLocation){
 		self.currentLocation(clickedLocation);
 		google.maps.event.trigger(clickedLocation.marker, 'click');
-		//toggleBounce.call(clickedLocation.marker);
 		console.log("hi");
 	};
 
@@ -163,8 +215,6 @@ var ViewModel = function (){
 }
 
 
-//bounce marker with list item clicked
-// info window
+
 //api in info window
-//error handling
 //mobile version
